caff9efc629162539740fb0aec2056382e6dd786
fox <a13821190779>
2017-10-17 15:35:37 +0800

    工具方法

---
+++ view/src/js/tools.js
@@ -0,0 +1,24 @@
+const debounce = (fn, threshhold, scope) => {
+	threshhold || (threshhold = 250)
+	let last,
+	deferTimer
+	return function () {
+		const context = scope || this
+		const now = +new Date,
+		args = arguments
+		if (last && now < last + threshhold) {
+			clearTimeout(deferTimer)
+			deferTimer = setTimeout(function () {
+				last = now
+				fn.apply(context, args)
+			}, threshhold)
+		} else {
+			last = now
+			fn.apply(context, args)
+		}
+	}
+}
+
+export default {
+	debounce
+}
